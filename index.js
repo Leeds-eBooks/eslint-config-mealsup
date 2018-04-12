7def40906825d904a0b6d5642775abab5d78b004
Ben Styles <enquiries@leeds-ebooks.co.uk>
2018-04-12 11:47:31 +0100

    add new react nules

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ module.exports = {
     'react-native/no-unused-styles': 1,
     'react-native/split-platform-components': 1,
     'react-native/no-inline-styles': 1,
+
     'react/jsx-key': 0,
     'react/jsx-boolean-value': 1,
     'react/prop-types': 0,
     'react/no-did-update-set-state': 1,
+    'react/no-access-state-in-setstate': 2,
+    'react/no-deprecated': 1,
+    'react/no-redundant-should-component-update': 1,
+    'react/no-typos': 1,
+    'react/no-this-in-sfc': 1,
+    'react/no-unused-state': 1,
+    'react/jsx-no-bind': 1,
   },
 }
