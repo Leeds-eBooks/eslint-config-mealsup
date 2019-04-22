fa0330d083c68d81a7ffd13c2c4ad1a8e2e04c2a
Ben Styles <enquiries@leeds-ebooks.co.uk>
2019-04-22 19:16:04 +0100

    Update dependencies and add some new rules

--- index.js
+++ index.js
@@ -3,29 +3,38 @@ module.exports = {
   globals: {
     __DEV__: false,
   },
-  plugins: ['react', 'react-native'],
+  plugins: ['react', 'react-native', 'react-hooks'],
   settings: {
     'import/resolver': {
       node: {
         extensions: ['.js', '.ios.js', '.android.js'],
       },
     },
+    react: {
+      version: 'detect',
+    },
   },
   rules: {
     'react-native/no-unused-styles': 1,
     'react-native/split-platform-components': 1,
     'react-native/no-inline-styles': 1,
+    'react-native/no-raw-text': 1,
 
     'react/jsx-key': 0,
     'react/jsx-boolean-value': 1,
+    'react/jsx-fragments': 1,
     'react/prop-types': 0,
     'react/no-did-update-set-state': 1,
     'react/no-access-state-in-setstate': 2,
     'react/no-deprecated': 1,
+    'react/no-unsafe': 1,
     'react/no-redundant-should-component-update': 1,
     'react/no-typos': 1,
     'react/no-this-in-sfc': 1,
     'react/no-unused-state': 1,
-    'react/jsx-no-bind': 1,
+    'react/jsx-no-bind': 0,
+
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
   },
 }
